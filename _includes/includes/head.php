0a96d2229c564ac07baaa7a0f986745cb2a05eed
Marc Durdin <marc@keyman.com>
2019-10-07 06:53:33 +1100

    Refresh analytics code

--- _includes/includes/head.php
+++ _includes/includes/head.php
@@ -29,19 +29,19 @@ require_once('servervars.php');
   <?php foreach($css as $cssFile){ ?>
     <link rel="stylesheet" type="text/css" href="<?php echo $cssFile; ?>">
   <?php } ?>
-  
+
   <link href='https://fonts.googleapis.com/css?family=Cabin:400,400italic,500,600,700,700italic|Source+Sans+Pro:400,700,900,600,300|Noto+Serif:400' rel='stylesheet' type='text/css'>
 
 	<script type="text/javascript">
 
 	 // Add a script element as a child of the body
-	 
+
 	 function downloadJS(src) {
 		 var element = document.createElement("script");
 		 element.src = src;
 		 (document.body ? document.body : document.head) .appendChild(element);
 	 }
-	 
+
 	 function downloadJSAtOnload() {
 		downloadJS("<?php echo cdn("js/jquery1-11-1.min.js"); ?>");
 		downloadJS("<?php echo cdn("js/bowser.min.js"); ?>");
@@ -51,38 +51,11 @@ require_once('servervars.php');
 		<?php } ?>
 	 }
 
-<?php 
-  echo empty($loadJQueryEarly) ? 
+<?php
+  echo empty($loadJQueryEarly) ?
     "window.addEventListener ? window.addEventListener('load', downloadJSAtOnload) : window.attachEvent('onload', downloadJSAtOnload);" :
-    "downloadJSAtOnload()"; 
+    "downloadJSAtOnload()";
 ?>
   </script>
-  <script>
-    function isIEOnWin81orEarlier() {
-      // If we are running IE11/or earlier on Windows 8.1 or earlier, then
-      // the autolink generates filenames that are misrecognised for download links,
-      // meaning users on those browesrs cannot easily download Keyman.
-      // Easiest way to test this is to test against navigator.userAgent
-      // Yes, we get some muddled analytics of cross-site of IE visitors but 
-      // it's a small enough percentage that we don't need to worry about it.
-      return (navigator.userAgent.indexOf('Trident/') >= 0 && navigator.userAgent.indexOf('Windows NT 6') >= 0) || 
-             (document.documentMode < 11);
-    }
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-249828-1',  'auto', {'allowLinker': !isIEOnWin81orEarlier()});
-    if(!isIEOnWin81orEarlier()) { 
-      ga('require', 'linker');
-      ga('linker:autoLink', 
-        ['keyman.com', 'www.keyman.com', 
-         'keymanweb.com', 'www.keymanweb.com', 
-         'help.keyman.com', 
-         'blog.keyman.com',
-        'www.tavultesoft.com', 'tavultesoft.com', 'secure.tavultesoft.com'] );
-    }
-    ga('send', 'pageview');
-  </script>
+  <?php require_once('includes/analytics.php'); ?>
 </head>
