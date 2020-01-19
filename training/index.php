f4636f13324edc5160bcfc986af907d607d4213c
Marc Durdin <marc@durdin.net>
2020-01-20 08:53:08 +1100

    feat: online training announcement

--- training/index.php
+++ training/index.php
@@ -10,11 +10,31 @@
 ?>
 <h2 class="red underline">Training Events</h2>
 
-<p>No training events are currently scheduled.</p>
+<h3 class="red underline">Online Keyman Developer Training Workshop - 24-28 February, 2020</h3>
 
+<p><a class='generic-cta-button' href='https://intre.org?487' target='_blank'>Register Now!</a></p>
+
+<p>We are hosting an Online Keyman Developer Training Workshop the week of February 24-28. We will teach you how to create powerful
+multi-platform keyboard layouts for desktop and mobile devices. By the end of the week, you will have a working keyboard layout
+for your language(s) ready to share with the world.</p>
+
+<p>To allow for a maximum range of participants from many time zones, we are only doing two hours of workshop each day, from 12:00-14:00 UTC.
+For those in the US, this will be early morning, after which they can spend the needed time working on their keyboard assignment.  For others,
+it will fall in the evening, and they can spend the following day doing their assignment before the next group session.  We will have support
+staff in various time zones standing by to help participants if needed.</p>
+
+<p>The cost is $30 which must be paid by bank card or SIL account number at our <a href='https://intre.org?487' target='_blank'>registration site</a>.
+The payment confirmation email will contain a link to register for the Zoom webinar, which will send you reminders as the day approaches.</p>
+
+<p><a href='https://intre.org/?487&amp;a=home' target='_blank'>Learn more...</a>
+
+<br>
 <br>
 
 <h3 class="red underline">Cancelled: Keyman Developer Training - Basel, Switzerland, February 2020</h3>
+
+<p>See the online workshop above for an alternative!</p>
+
 <p>
 Due to insufficient numbers, we have cancelled this workshop. We will continue to look at ways to make this training available
 and will provide updates via the Keyman mailing list and website.  We hope that in the near future we will be able to provide
