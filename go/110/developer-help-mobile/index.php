fca4d903d443ba27ab784198ae7754fde54a8741
Darcy Wong <darcy_wong@sil.org>
2020-02-18 16:07:07 +0700

    Refactor package guide redirects to web.config

--- go/110/developer-help-mobile/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    header('Location: https://help.keyman.com/developer/11.0/guides/distribute/packages');
-?>
--- go/120/developer-help-packages/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    header('Location: https://help.keyman.com/developer/12.0/guides/distribute/packages');
-?>
