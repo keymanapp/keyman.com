b1d0522506d7c718fbbeadea66a253c506f69935
Darcy Wong <darcy_wong@sil.org>
2022-02-02 15:47:07 +0700

    chore: Update feature list from Retrospective document

--- 15/index.php
+++ 15/index.php
@@ -31,10 +31,62 @@ head([
 <p>These major features are in all supported platforms:</p>
 <ul>
   <li>
-    Moved crash reporting to sentry.io
+    Infrastructure for Rust - for future LDML keyboard support (#5162)
+  </li>
+  <li>
+    Infrastructure for WASM - for future LDML keyboard support (#5233)
+  </li>
+  <li>
+    Caps Lock Layer + double-tap gesture - touch platforms (#5988, #5989)
+  </li>
+  <li>
+    Support for Caps-lock stores (capsAlwaysOff, shiftFreesCaps, capsOnOnly) (#5432, #5519)
+  </li>
+  <li>
+    Start of Sentence detection - touch platforms (#5963)
+  </li>
+  <li>
+    Inline On Screen Keyboard (#5665)
+  </li>
+  <li>
+    Improved security in incxstr / decxstr
   </li>
   <li>
     Many bug fixes and improvements (see the <a href="<?= KeymanHosts::Instance()->help_keyman_com ?>/version-history">version history</a>)
+  </li>
+</ul>
+<br>
+<p>New Localizations - Many Nigerian languages provided by <a href='https://translatorswithoutborders.org/'>Translators Without Borders</a></p>
+<ul>
+  <li>
+    Bura-Pabir
+  </li>
+  <li>
+    Demgal Fulfulde
+    </li>
+  <li>
+    Hausa
+  </li>
+  <li>
+    Kanuri
+  </li>
+  <li>
+    Kibaku
+  </li>
+  <li>
+    Mandala (Wandala)
+  </li>
+  <li>
+    Marghi
+  </li>
+  <li>
+    Shuwa (Latin)
+  </li>
+  <li>
+    Waha
+  </li>
+  <li>
+    Simplified Chinese
   </li>
 </ul>
 <br>
@@ -64,7 +116,7 @@ downloadSection('Keyman 15 for Windows',   'windows',     'keyman-$version.exe',
 <h3>What's New in Keyman 15 for Windows?</h3>
 
 <ul>
-  <li>Keyman Core integration (#5443)</li>
+  <li>Use Keyman Core internally (#5443)</li>
 </ul>
 
 
@@ -89,8 +141,15 @@ downloadSection('Keyman 15 for macOS',   'mac',     'keyman-$version.dmg', 'beta
 
 <ul>
   <li>Now supports Ubuntu 21.04 (Hirsute), 21.10 (Impish), and 22.04 (Jammy)</li>
-  <li>Consolidated Debian source packages from three to one source package: keyman (#5022)</li>
+  <li>Improvements in Debian packaging (#5022)</li>
   <li>Fix crash when displaying certain keyboard details (#5758)</li>
+  <li>Caps Lock Stores (#5497)</li>
+  <li>Support for Ubuntu Jammy 22.04 (#6037) and Impish 21.10 (#5334)</li>
+  <li>fcitx5 integration (#5215)</li>
+  <li>Improve installation of packages in shared location (#6015)</li>
+  <li>Improvements in Debian packaging</li>
+  <li>Automated integration tests with Keyman Core</li>
+  <li>Lots of bugfixes</li>
 </ul>
 
 <?php
@@ -102,6 +161,8 @@ downloadSection('Keyman 15 for Android', 'android', 'keyman-$version.apk', 'beta
 <h3>What's new in Keyman 15 for Android?</h3>
 
 <ul>
+  <li>Keyman Engine no longer needs internet access </li>
+  <li>English keyboard can now be removed (#5838)</li>
   <li>Add a menu to adjust keyboard height (#5606)</li>
   <li>Add a settings option to change the displayed keyboard name on the spacebar</li>
   <li>Improve the globe key experience for switching keyboards (#5437, #5973):
@@ -111,8 +172,8 @@ downloadSection('Keyman 15 for Android', 'android', 'keyman-$version.apk', 'beta
       <li>Allow switching to other system IME's in the keyboard picker menu</li>
     </ol>
   </li>
-  <li>Allow uninstall of sil_euro_latin keyboard (#5838)</li>
   <li>Select numeric layer when entering a number field (#5664)</li>
+  <li>Improved performance (#5376)</li>
 </ul>
 
 <h2 id='ios' class="red underline">Keyman 15 for iPhone and iPad</h2>
@@ -122,7 +183,7 @@ downloadSection('Keyman 15 for Android', 'android', 'keyman-$version.apk', 'beta
 <h3>What's new in Keyman 15 for iPhone and iPad?</h3>
 
 <ul>
-  <li>Add controls for spacebar caption (#5365)</li>
+  <li>General polish, bug fixes, and performance improvments</li>
   <li>Update minimum iOS version to 12.1 (#5165)</li>
 </ul>
 
@@ -137,10 +198,20 @@ downloadSection('KeymanWeb 15', 'web', 'keymanweb-$version.zip', 'beta');
 <h3>What's New in KeymanWeb 15?</h3>
 
 <ul>
-  <li>Imrpove keyboard switch performance (#5958)</li>
+  <li>Refactored OSK Core</li>
+  <li>Promise-based API (#5389, #5260, #5121)</li>
+  <li>Improve keyboard switch performance (#5958)</li>
   <li>Enable mouse interaction for the predictive banner (#5739)</li>
 </ul>
+<br>
 
+<h3>What's New in Infrastructure and Build?</h3>
+<ul>
+  <li>Many incremental improvements to status.keyman.com</li>
+  <li>Gradual update of <a href='https://help.keyman.com/'>help site</a> to Markdown</li>
+  <li>Various other bug fixes, polish</li>
+</ul>
+<br>
 
 
 <h1 class='red underline'>Developer Software</h1>
@@ -152,14 +223,16 @@ downloadSection('Keyman Developer 15',    'developer', 'keymandeveloper-$version
 <h3>What's new in Keyman Developer 15?</h3>
 
 <ul>
-  <li>Debugger uses Keyman Core (#5513)</li>
+  <li>Core-based debugger (#5425, #5448, #5513)</li>
+  <li>Keyman Developer Server (#6033,#6034,#6035,#6036)</li>
+  <li>Improved native-mode debugger (#5696, #5640, #5647)</li>
+  <li>Web test no longer needs Developer tools for touch testing (#5723)</li>
   <li>
     Add support for <code class='language-java'>U_xxxx_yyyy_...</code> 
     indentifiers so you can emit more than a single Unicode character with 
     touch keys without additional kmn code (#5894)
   </li>
   <li>Keyman Developer debugger no longer depends on Keyman for Windows (#5588)</li>
-  <li>Add support for testing touch layouts without requiring use of device emulation (#5723)</li>
   <li>live reload of web debugger (#6035)</li>
 </ul>
 
