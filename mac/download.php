5f663e502a59afde4c915e9eae90e5c90abc1251
Marc Durdin <marc@durdin.net>
2022-05-21 10:11:12 +1000

    feat: mac homebrew install instructions

--- mac/download.php
+++ mac/download.php
@@ -5,19 +5,31 @@
   // Required
   head([
     'title' =>'Download Keyman for macOS',
-    'css' => ['template.css','index.css','desktop-download.css'],
-    'js' => ['download.js'],
+    'css' => ['template.css','index.css','desktop-download.css','prism.css'],
+    'js' => ['download.js','prism.js'],
     'showMenu' => true
   ]);
 ?>
 <h2 class="red underline">Download Keyman for macOS</h2>
 <p>
-    Keyman is a program that reconfigures your keyboard to type in another language. This download provides a simple installer for Keyman.
+    Keyman is a program that reconfigures your keyboard to type in another language. This download provides a simple installer for Keyman on macOS.
 </p>
 <p>
     Keyman <?php echo $stable_version; ?> for macOS is compatible with OS X Yosemite (10.10) and later.
 </p>
 
+<h3 class="red underline">Install directly from keyman.com</h3>
+
 <?php
   downloadLargeCTA('Keyman for macOS', 'mac', 'stable', 'keyman-$version.dmg');
 ?>
+
+<h3 class="red underline">Install via Homebrew</h3>
+<p>You can also install Keyman with <a href='https://brew.sh'>Homebrew</a>:</p>
+
+<pre><code class='language-shell'>brew install --cask keyman</code></pre>
+
+<p>When installing Keyman with Homebrew, you must manually add the Keyman
+   input source under:</p>
+
+<p><b>Preferences → Keyboard → Input Sources</b></p>
