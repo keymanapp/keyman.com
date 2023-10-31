bdae5876a0c9adbc3719ab10469aaf2046d6ff28
Darcy Wong <darcy_wong@sil.org>
2023-10-31 07:23:42 +0700

    chore: undo comments

--- build.sh
+++ build.sh
@@ -33,9 +33,9 @@ function test_docker_container() {
   echo "---- Testing links ----"
   set +e;
   set +o pipefail;
-  npx broken-link-checker http://localhost:8053/_test --recursive --ordered ---host-requests 50 -e --filter-level 3 #| \
-    #grep -E "BROKEN|Getting links from" | \
-    #grep -B 1 "BROKEN";
+  npx broken-link-checker http://localhost:8053/_test --recursive --ordered ---host-requests 50 -e --filter-level 3 | \
+    grep -E "BROKEN|Getting links from" | \
+    grep -B 1 "BROKEN";
 
   echo "Done checking links"
   rm tier.txt
