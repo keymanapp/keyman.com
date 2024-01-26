ae365da28e6011e0a31890bf4377581dfa7a9d1f
Darcy Wong <darcy_wong@sil.org>
2024-01-26 10:03:36 +0700

    chore: Add _control/ready

---
+++ _control/ready.php
@@ -0,0 +1,15 @@
+<?php
+  // Test web server ready, and _common files, and vendor files ready
+  if (!is_dir(__DIR__ . '/../cdn/deploy')) {
+    die("/cdn/deploy not ready");
+  }
+  
+  if (!file_exists(__DIR__ . '/../_common/KeymanHosts.php')) {
+    die('/_common not ready');
+  }
+  
+  if (!is_dir(__DIR__ . '/../vendor')) {
+    die('/vendor not ready');
+  }
+
+  echo "ready\n";
