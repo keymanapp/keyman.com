24772ca8d5b27eb616c0a151773a02a72d992973
Meng-Heng <havmengheng111@gmail.com>
2024-08-15 13:28:11 +0700

    chore: logos randomizer for keyman.com

---
+++ _includes/2020/ImageRandomizer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Keyman\Site\com\keyman;
+
+class ImageRandomizer {
+    
+    static function randomizer($folder) { 
+        if(!empty($_SERVER['DOCUMENT_ROOT']) && !empty($folder)) {
+          $imgDir = $_SERVER['DOCUMENT_ROOT'] . '/cdn/dev/' . $folder;
+          $img = glob($imgDir . '*.{png,jpg,jpeg,gif}', GLOB_BRACE);
+          if(!empty($img)) {
+            shuffle($img);
+            return str_replace($_SERVER['DOCUMENT_ROOT'], '',  $img[0]);
+          }
+        }
+        return "";
+      }
+}
+  
+
+?>
