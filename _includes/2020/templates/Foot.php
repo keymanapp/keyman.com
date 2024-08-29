bf34da1650463ca8b5567ac1bfdf8de0b0d4e13a
Meng-Heng <havmengheng111@gmail.com>
2024-08-29 12:59:53 +0700

    build.sh

--- _includes/2020/templates/Foot.php
+++ _includes/2020/templates/Foot.php
@@ -59,7 +59,7 @@
         </div>
         <div class="footer-third sil-logo">
             <br>
-            <a href="/about/"><img id="sil-logo" src="<?php echo Util::cdn("img/sil-logo-blue-2017_1.png"); ?>" alt='SIL' /></a>
+            <a href="/about/"><img id="sil-logo" src="<?php echo ImageRandomizer::randomizer("img/sil-logos-2024/"); ?>" width="50%" alt='SIL' /></a>
             <p>Created by <a href="/about/">SIL Global</a></p>
         </div>
     </div>
