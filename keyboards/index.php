60f4886478681932ab11f502c0a32d9067ee14c1
Darcy Wong <darcy_wong@sil.org>
2024-11-13 09:49:48 +0700

    fix locale passed to page description

--- keyboards/index.php
+++ keyboards/index.php
@@ -20,7 +20,7 @@
   $head_options = [
     'title' => _("Keyboard Search"),
     'description' => _("Keyman Keyboard Search"),
-    'language' => $locale,
+    'language' => $embed_locale,
     'css' => [Util::cdn('css/template.css'), Util::cdn('keyboard-search/search.css')],
     'js' => [Util::cdn('keyboard-search/jquery.mark.js'), Util::cdn('keyboard-search/dedicated-landing-pages.js'),
       Util::cdn('keyboard-search/search.js')]
