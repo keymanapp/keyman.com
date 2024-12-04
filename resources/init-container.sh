d34b47bb6f80d43d0ba00d8407de6c5b14d33c74
Darcy Wong <darcy_wong@sil.org>
2024-12-04 11:12:14 +0700

    feat: Configure en_US locale in Dockerfile

--- resources/init-container.sh
+++ resources/init-container.sh
@@ -10,3 +10,20 @@ else
   echo "Skip Generating CDN and clean CDN cache"
   rm -rf cdn/deploy
 fi
+
+echo "---- Generating .mo localization files ----"
+cd _includes/locale/en/LC_MESSAGES/
+
+# cleanup previous .mo files
+find . -type f -name '*.mo' -delete
+
+# Compile .po to .mo localization files
+for filename in `find . -type f -name "*.po"`; do
+  # Remove the .po extension
+  base_name=`echo ${filename} | sed 's/\.[^.]*$//'`
+  msgfmt "${base_name}.po" --output-file="${base_name}".mo
+  retVal=$?
+  if [[ ${retVal} -ne 0 ]]; then
+    exit 1
+  fi
+done
