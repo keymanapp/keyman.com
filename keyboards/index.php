19510be2cdf72be569a637f3d568a2da123f4426
Darcy Wong <darcy_wong@sil.org>
2025-01-20 10:39:00 +0700

    Simplify strings
    * Replace array of indexes with `_()` alias

--- keyboards/index.php
+++ keyboards/index.php
@@ -9,34 +9,11 @@
   use Keyman\Site\com\keyman\templates\Foot;
   use Keyman\Site\com\keyman\Locale;
 
-  // Of array of strings at top of file
-  // by msgid 
-  $keyboardIndexStrings = Locale::localize('keyboards', [
-    'Keyboard Search',
-    'Keyman Keyboard Search',
-    'Keyboard search:',
-    'Enter language or keyboard',
-    'Search',
-    'New search',
-    'Enter the name of a keyboard or language to search for%1$s',
-    'Popular keyboards',
-    'All keyboards',
-    'Hints',
-    'The search always returns a list of keyboards. It searches for keyboard names and details, language names, country names and script names.',
-    'You can apply prefixes',
-    '(keyboards)',
-    '(languages)',
-    '(scripts, writing systems) or',
-    '(countries) to filter your search results. For example',
-    'searches for keyboards for languages used in Thailand.',
-    'Use prefix',
-    'to search for a BCP 47 language tag, for example',
-    'searches for Tigrigna (Ethiopia)',
-  ]);
+  Locale::localize('keyboards');
 
   $head_options = [
-    'title' => $keyboardIndexStrings['Keyboard Search'],
-    'description' => $keyboardIndexStrings['Keyman Keyboard Search'],
+    'title' => _('Keyboard Search'),
+    'description' => _('Keyman Keyboard Search'),
     'language' => Locale::currentLocale(),
     'css' => [Util::cdn('css/template.css'), Util::cdn('keyboard-search/search.css')],
     'js' => [Util::cdn('keyboard-search/jquery.mark.js'), Util::cdn('keyboard-search/dedicated-landing-pages.js'),
@@ -73,14 +50,14 @@
 
 <div class='<?= $embed == 'none' ? '' : 'embed embed-'.$embed ?>'>
 
-  <h2 class="red underline"><a href='/keyboards'><?= $keyboardIndexStrings['Keyboard Search'] ?></a></h2>
+  <h2 class="red underline"><a href='/keyboards'><?= _('Keyboard Search') ?></a></h2>
 
   <div id='search-box'>
     <form method='get' action='/keyboards' name='f'>
-      <label for="search-q"><?= $keyboardIndexStrings['Keyboard search:'] ?></label><input id="search-q" type="text" placeholder="<?= $keyboardIndexStrings['Enter language or keyboard'] ?>" name="q"
+      <label for="search-q"><?= _('Keyboard search:') ?></label><input id="search-q" type="text" placeholder="<?= _('Enter language or keyboard') ?>" name="q"
       <?php if($embed == 'none') echo 'autofocus'; ?>>
-      <input id="search-f" type="image" src="<?= cdn('img/search-button.png"') ?>" value="<?= $keyboardIndexStrings['Search'] ?>" onclick="return do_search()">
-      <label id="search-new"><a href='/keyboards<?=$session_query_q?>'><?= $keyboardIndexStrings['New search'] ?></a></label>
+      <input id="search-f" type="image" src="<?= cdn('img/search-button.png"') ?>" value="<?= _('Search') ?>" onclick="return do_search()">
+      <label id="search-new"><a href='/keyboards<?=$session_query_q?>'><?= _('New search') ?></a></label>
       <input id="search-obsolete" type="hidden" name="obsolete" value="0">
       <input id="search-page" type="hidden" name="page" value="1">
     </form>
@@ -90,29 +67,29 @@
   <div id='search-results'></div>
   <div id='search-results-empty'>
     <p>
-      <?= Locale::_s($keyboardIndexStrings['Enter the name of a keyboard or language to search for%1$s'], ". (") ?>
-      <a href="?q=p:popular"><?= $keyboardIndexStrings['Popular keyboards'] ?></a> | 
-      <a href="?q=p:alphabetical"><?= $keyboardIndexStrings['All keyboards'] ?></a>)
+      <?= _('Enter the name of a keyboard or language to search for') ?> (
+      <a href="?q=p:popular"><?= _('Popular keyboards') ?></a> | 
+      <a href="?q=p:alphabetical"><?= _('All keyboards') ?></a>)
     </p>
     <br />
-    <p><?= $keyboardIndexStrings['Hints'] ?></p>
+    <p><?= _('Hints') ?></p>
     <ul>
       <li>
-        <?= $keyboardIndexStrings['The search always returns a list of keyboards. ' .
-          'It searches for keyboard names and details, language names, country names and script names.'] ?>
+        <?= _('The search always returns a list of keyboards. ' .
+          'It searches for keyboard names and details, language names, country names and script names.') ?>
       </li>
       <li>
-        <?= $keyboardIndexStrings['You can apply prefixes'] ?> 
-        <code>k:</code> <?= $keyboardIndexStrings['(keyboards)'] ?>
-        <code>l:</code> <?= $keyboardIndexStrings['(languages)'] ?>
-        <code>s:</code> <?= $keyboardIndexStrings['(scripts, writing systems) or'] ?>
-        <code>c:</code> <?= $keyboardIndexStrings['(countries) to filter your search results. For example'] ?> 
-        <code>c:thailand</code> <?= $keyboardIndexStrings['searches for keyboards for languages used in Thailand.'] ?>
+        <?= _('You can apply prefixes') ?> 
+        <code>k:</code> <?= _('(keyboards)') ?>
+        <code>l:</code> <?= _('(languages)') ?>
+        <code>s:</code> <?= _('(scripts, writing systems) or') ?>
+        <code>c:</code> <?= _('(countries) to filter your search results. For example') ?> 
+        <code>c:thailand</code> <?= _('searches for keyboards for languages used in Thailand.') ?>
       </li>
       <li>
-        <?= $keyboardIndexStrings['Use prefix'] ?> 
-        <code>l:id:</code> <?= $keyboardIndexStrings['to search for a BCP 47 language tag, for example'] ?> 
-        <code>l:id:ti-et</code> <?= $keyboardIndexStrings['searches for Tigrigna (Ethiopia)'] ?>
+        <?= _('Use prefix') ?> 
+        <code>l:id:</code> <?= _('to search for a BCP 47 language tag, for example') ?> 
+        <code>l:id:ti-et</code> <?= _('searches for Tigrigna (Ethiopia)') ?>
       </li>
     </ul>
   </div>
