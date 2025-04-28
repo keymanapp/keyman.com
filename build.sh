6206dc8acb76004d747b1c4cb9fd6e7bea26ecae
Darcy Wong <darcy_wong@sil.org>
2025-04-28 09:31:12 +0700

    chore: refactor tests
    * Add PHP unit test step
    * Configure Docker image to install phpunit when BUILDER_CONFIGURATION=debug

--- build.sh
+++ build.sh
@@ -28,11 +28,22 @@ builder_describe \
 builder_parse "$@"
 
 function test_docker_container() {
-  # TODO: lint tests
+  # Note: ci.yml replicates these
+
   echo "TIER_TEST" > tier.txt
-  echo "---- Testing links ----"
   set +e;
   set +o pipefail;
+
+  builder_echo blue "---- PHP unit tests"
+  docker exec $KEYMAN_CONTAINER_DESC sh -c "vendor/bin/phpunit --testdox"
+
+  # Lint .php files for obvious errors
+  builder_echo blue "---- Lint PHP files"
+  docker exec $KEYMAN_CONTAINER_DESC sh -c "find . -name '*.php' | grep -v '/vendor/' | xargs -n 1 -d '\\n' php -l"
+
+  # NOTE: link checker runs on host rather than in docker image
+  #--exclude '*/downloads/releases/*' | \ 
+  builder_echo blue "---- Testing links"
   npx broken-link-checker http://localhost:8053/_test --recursive --ordered ---host-requests 50 -e --filter-level 3 --exclude '*/donate' | tee blc.log
   local BLC_RESULT=${PIPESTATUS[0]}
   echo ----------------------------------------------------------------------
@@ -42,7 +53,7 @@ function test_docker_container() {
     grep -E "BROKEN|Getting links from" | \
     grep -B 1 "BROKEN";
 
-  echo "Done checking links"
+  builder_echo blue "Done checking links"
   rm tier.txt
   return "${BLC_RESULT}"
 }
@@ -50,7 +61,7 @@ function test_docker_container() {
 builder_run_action configure  bootstrap_configure
 builder_run_action clean      clean_docker_container $KEYMAN_IMAGE_NAME $KEYMAN_CONTAINER_NAME
 builder_run_action stop       stop_docker_container  $KEYMAN_IMAGE_NAME $KEYMAN_CONTAINER_NAME
-builder_run_action build      build_docker_container $KEYMAN_IMAGE_NAME $KEYMAN_CONTAINER_NAME
+builder_run_action build      build_docker_container $KEYMAN_IMAGE_NAME $KEYMAN_CONTAINER_NAME $BUILDER_CONFIGURATION
 builder_run_action start      start_docker_container $KEYMAN_IMAGE_NAME $KEYMAN_CONTAINER_NAME $KEYMAN_CONTAINER_DESC $HOST_KEYMAN_COM $PORT_KEYMAN_COM $BUILDER_CONFIGURATION
 
 builder_run_action test       test_docker_container
