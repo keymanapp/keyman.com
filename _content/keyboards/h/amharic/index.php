5c1970b6d7883e65f2e360c03f8d6539cceb087d
Darcy Wong <darcy_wong@sil.org>
2026-04-06 18:57:19 +0700

    try to fix more keyboard landing links

--- _content/keyboards/h/amharic/index.php
+++ _content/keyboards/h/amharic/index.php
@@ -113,7 +113,7 @@
     </a>
   </div>
   <div class="download-cta-small Bookmarklet" id="cta-Bookmarklet">
-    <a href="../../../../bookmarklet/">
+    <a href="../../../bookmarklet/">
       <img src="<?php echo cdn('img/cta-icons/icon-bookmarklet.png'); ?>" />
       <p>Bookmarklet</p>
     </a>
