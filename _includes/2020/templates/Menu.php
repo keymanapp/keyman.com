7566f6b326bd7488cea8b84b29250a88c2f5dbed
Darcy Wong <darcy_wong@sil.org>
2026-04-21 11:07:48 +0700

    Apply suggestion from @darcywong00

--- _includes/2020/templates/Menu.php
+++ _includes/2020/templates/Menu.php
@@ -301,7 +301,7 @@ END;
                             <li><a href="/<?=$fields->lang?>/free/">Free on all Platforms</a></li>
                             <li><a href="/<?=$fields->lang?>/ldml/">LDML Support</a></li>
                             <li><a href="<?= KeymanHosts::Instance()->help_keyman_com ?>">Help and Documentation</a></li>
-                            <li><a href="/contact/">Contact Us</a></li>
+                            <li><a href="/<?=$fields->lang?>/contact/">Contact Us</a></li>
                             <li><a href="<?= KeymanHosts::Instance()->blog_keyman_com ?>">Keyman Blog</a></li>
                             <li><a href="/<?=$fields->lang?>/testimonials/">Testimonials</a></li>
                         </ul>
