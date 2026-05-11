00bade108e01a478e24ae9806bbfe53d0c2975b8
Marc Durdin <marc@durdin.net>
2026-05-11 11:19:40 +0200

    chore: remove legacy keyboard search
    Fixes: #726

--- windows/legacy-keyboard-download.php
+++ windows/legacy-keyboard-download.php
@@ -1,29 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset='utf-8'>
-  <title>Keep in touch</title>
+  <title>Keyboard downloads</title>
 </head>
 <body>
 <div>
 
 <!--
-  This page is the replacement for the embedded "keep in touch" for Keyman Desktop 13.0
+  This page is the replacement for the embedded legacy keyboard search for Keyman Desktop 13.0
   and earlier versions (Windows-only). It runs in an embedded Internet Explorer frame.
   This page intentionally does not include any of the shared resources such as Sentry, as
   none of those shared resources are compatible with IE11 or earlier versions.
 
-  The app loads https://keyman.com/go/desktop/keep-in-touch, which redirects to this holding
+  The app loads https://keyman.com/go/desktop/download-keyboards, which redirects to this holding
   page.
 -->
 
-  <h2 class="red underline">Keep in touch</a></h2>
+  <h2 class="red underline">Keyboard downloads</a></h2>
 
   <p>You are running a very old version of Keyman. We recommend that you <a href='/windows' target='_blank'>upgrade to the latest version</a> for
   security, stability and compatibility updates.</p>
 
-  <p>Please visit <a href='/contact' target='_blank'>Keep in Touch</a> for contact links for the Keyman team.</p>
+  <p>To download a keyboard for this version of Keyman, please visit <a href='/keyboards' target='_blank'>Keyman keyboard search</a></p>
 </div>
 </body>
 </html>
-
